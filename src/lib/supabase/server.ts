5fc8c794236984e30586730e2e034aab88a74dbf
khulsebosch <kth134@gmail.com>
2026-01-04 20:02:29 -0600

    Migrate to @supabase/ssr for auth

---
+++ src/lib/supabase/server.ts
@@ -0,0 +1,10 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+export const supabaseServer = () => {
+  return createServerComponentClient({
+    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    cookies,
+  })
+}
